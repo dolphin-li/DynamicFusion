c561b9884a85744c713cda3e0767119f4ee87916
Dongping Li <dong_pingli@hotmail.com>
2015-09-26 18:44:06 +0800

    cached_allocater updated. fixed out_of_memory issue.

--- algorithm/DynamicFusion/cudpp/thrust_wrapper.cu
+++ algorithm/DynamicFusion/cudpp/thrust_wrapper.cu
@@ -22,19 +22,44 @@
 #include <thrust/sort.h>
 #include <thrust/pair.h>
 #include <map>
+#include <algorithm>
 #include "thrust_wrapper.h"
 
 namespace thrust_wrapper
 {
+	// Exponentially spaced buckets.
+	static const int NumBuckets = 84;
+	const size_t BucketSizes[NumBuckets] = {
+		256, 512, 1024, 2048, 4096, 8192,
+		12288, 16384, 24576, 32768, 49152, 65536,
+		98304, 131072, 174848, 218624, 262144, 349696,
+		436992, 524288, 655360, 786432, 917504, 1048576,
+		1310720, 1572864, 1835008, 2097152, 2516736, 2936064,
+		3355648, 3774976, 4194304, 4893440, 5592576, 6291456,
+		6990592, 7689728, 8388608, 9786880, 11184896, 12582912,
+		13981184, 15379200, 16777216, 18874368, 20971520, 23068672,
+		25165824, 27262976, 29360128, 31457280, 33554432, 36910080,
+		40265472, 43620864, 46976256, 50331648, 53687296, 57042688,
+		60398080, 63753472, 67108864, 72701440, 78293760, 83886080,
+		89478656, 95070976, 100663296, 106255872, 111848192, 117440512,
+		123033088, 128625408, 134217728, 143804928, 153391872, 162978816,
+		172565760, 182152704, 191739648, 201326592, 210913792, 220500736
+	};
+
+	static int LocateBucket(size_t size) {
+		if (size > BucketSizes[NumBuckets - 1])
+			return -1;
+
+		return (int)(std::lower_bound(BucketSizes, BucketSizes + NumBuckets, size) -
+			BucketSizes);
+	}
+
 	// cached_allocator: a simple allocator for caching allocation requests
 	class cached_allocator
 	{
 	public:
 		// just allocate bytes
 		typedef char value_type;
-		enum{
-			BlockSize = 512
-		};
 
 		cached_allocator() {}
 
@@ -48,10 +73,15 @@ namespace thrust_wrapper
 		{
 			char *result = 0;
 
-			int nBlocks = (num_bytes+BlockSize-1)/BlockSize;
+
+			int pos = LocateBucket(num_bytes);
+			if (pos < 0 || pos >= NumBuckets)
+				throw::std::exception("error: not supported size in thrust_wrapper::cached_allocator()");
+
+			int nAllocate = BucketSizes[pos];
 
 			// search the cache for a free block
-			free_blocks_type::iterator free_block = free_blocks.find(nBlocks);
+			free_blocks_type::iterator free_block = free_blocks.find(nAllocate);
 
 			if (free_block != free_blocks.end())
 			{
@@ -73,7 +103,7 @@ namespace thrust_wrapper
 					//std::cout << "cached_allocator::allocator(): no free block found; calling cuda::malloc" << std::endl;
 
 					// allocate memory and convert cuda::pointer to raw pointer
-					result = thrust::cuda::malloc<char>(nBlocks*BlockSize).get();
+					result = thrust::cuda::malloc<char>(nAllocate).get();
 				}
 				catch (std::runtime_error &e)
 				{
@@ -82,7 +112,7 @@ namespace thrust_wrapper
 			}
 
 			// insert the allocated pointer into the allocated_blocks map
-			allocated_blocks.insert(std::make_pair(result, nBlocks));
+			allocated_blocks.insert(std::make_pair(result, nAllocate));
 
 			return result;
 		}
